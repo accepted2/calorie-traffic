f60c60fda303acebd69a8271cd7088394fa269b9
accepted2 <saleksejgej@gmail.com>
2025-01-02 08:43:15 +0200

    Initial commit

--- myapp/settings.py
+++ myapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-uckhxo7d82!k==^$j5i-mtwuq71()bwube7kumpcrbk99v8((n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
